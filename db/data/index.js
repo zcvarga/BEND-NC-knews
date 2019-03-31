843582290cf1c2b4c1ff69399c3952d49d6ad4dd
Zsuzsi Varga <zsuzsivarga@zsuzsis-mbp.home>
2019-03-31 21:57:43 +0100

    added knexfile v3

--- db/data/index.js
+++ db/data/index.js
@@ -1,8 +1,8 @@
 const test = require('./test-data');
 const development = require('./development-data');
-
+const production = require('./development-data');
 const env = process.env.NODE_ENV || 'development';
 
-const data = { test, development };
+const data = { test, development, production };
 
 module.exports = data[env];
